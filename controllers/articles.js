bccee99bc1df8c3d086a47c8a3c64334ad892559
Kelli Kirk <kelli.kirk@voco.ee>
2025-02-01 12:22:43 +0200

    Display all articles

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,7 +1,7 @@
 const db = require('../utils/db')
 
 const getAllArticles = (req, res) => {
-    let sql = 'SELECT * FROM ARTICLE'
+    let sql = 'SELECT * FROM article'
     db.query(sql, (error, result) => {
         res.render('index',{
             articles: result
