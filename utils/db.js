5299c2854c6186bd754c00985a01a6cca6d7ed68
Kelli Kirk <kelli.kirk@voco.ee>
2026-02-05 11:37:43 +0200

    Get all articles from database feature

--- utils/db.js
+++ utils/db.js
@@ -3,9 +3,10 @@ const mysql = require('mysql2')
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'qwerty',
+    password: 'Qwerty1!',
     database: 'joga_mysql'
 } )
+
 
 connection.connect((err) => {
     if(err) throw err
